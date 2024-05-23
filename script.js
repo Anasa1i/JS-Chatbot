285f6940a323d2b9b7a40f4443d2250c1f9fd797
Anas Ali <anasalimgmt@gmail.com>
2024-05-23 08:18:46 +0500

    update

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+/////// START NOW BUTTON js   ///////////
+
+// document.getElementById("init");
+// var cbots = document.getElementById("chatBox");
+// // var len1 = data.chatinit.title.length
+
+// function showChatBot() {
+//     console.log(this.innerText);
+//     if(this.innerText === 'START CHATBOT NOW'){
+//         document.getElementById('test').style.display='block'
+//         document.getElementById('init').style.innerText='CLOSE CHAT'
+//     }
+//     else{
+//         location.reload()
+//     }
+// }
+
+
 ///////           CHATBOT JAVASCRIPT         ///////////////
 
-function sendMesasge() {
-    console.log("hello")
-}
+// function sendMesasge() {
+//     console.log("hello")
+// }
 
-var list = document.getElementById("list")
+var chatBox = document.getElementById("chatBox")
 var typing = document.getElementById("typing")
 function sendMessage() {
     // console.log(event.keyCode)
@@ -13,7 +31,7 @@ function sendMessage() {
         var elem = event.target;
 
         // back to the this line
-        list.innerHTML += "<li class='right-side'>" + elem.value + "</li>"
+        chatBox.innerHTML += "<li class='right-side'>" + elem.value + "</li>"
 
         typing.style.display = "block";
 
@@ -23,32 +41,91 @@ function sendMessage() {
                 
             setTimeout(function (){
 
-                // list.style.backgroundColor = "red"
+                // chatBox.style.background = "red"
 
-                list.innerHTML += "<li class='left-side'>  Hello </li>"
-            }, 1500) // time ke andr fucction time ko daldiya
+                chatBox.innerHTML += "<li class='left-side'>Hey, </li>"
+            }, 2500) // time ke andr fucction time ko daldiya
 
 
             setTimeout(function (){
-                list.innerHTML += "<li class='left-side'>  how are you? </li>";
+                chatBox.innerHTML += "<li class='left-side'>Mein SMIT student ka ek chatbot hoon. Mere paas kuch Saylani Welfare Trust ki information hai, main ismein aapki Kya madad kar sakta hoon?</li>";
                 typing.style.display = "none";
-            }, 2500) 
+            }, 1500) 
 
 
         } // reply ka js code ,,, event.target.value === 'hi' - brabr ho 'hi' ke
+          
+
+
+        // first else
 
         else if (['thek','thk','thik','theek', 'alhamdulillah','mast','fine','set','fit'].indexOf(elem.value.toLowerCase()) !== -1){
             setTimeout(function (){
 
-                list.innerHTML += "<li class='left-side'> Mei Apki Kia Madad Krskhta hun</li>"
+                chatBox.innerHTML += "<li class='left-side'> Mei Apki Kia Madad Krskhta hun</li>"
                 typing.style.display = "none";
             }, 1500)
         }
 
+
+
+        else if (['bashir','thk','thik','theek', 'alhamdulillah','mast','fine','set','fit'].indexOf(elem.value.toLowerCase()) !== -1){
+            setTimeout(function (){
+
+                chatBox.innerHTML += "<li class='left-side'> Mei Apki Kia Madad Krskhta hun</li>"
+                typing.style.display = "none";
+            }, 1500)
+        }
+
+
+
+        else if (['thek','thk','thik','theek', 'alhamdulillah','mast','fine','set','fit'].indexOf(elem.value.toLowerCase()) !== -1){
+            setTimeout(function (){
+
+                chatBox.innerHTML += "<li class='left-side'> Mei Apki Kia Madad Krskhta hun</li>"
+                typing.style.display = "none";
+            }, 1500)
+        }
+
+
+        else if (['thek','thk','thik','theek', 'alhamdulillah','mast','fine','set','fit'].indexOf(elem.value.toLowerCase()) !== -1){
+            setTimeout(function (){
+
+                chatBox.innerHTML += "<li class='left-side'> Mei Apki Kia Madad Krskhta hun</li>"
+                typing.style.display = "none";
+            }, 1500)
+        }
+
+
+        else if (['thek','thk','thik','theek', 'alhamdulillah','mast','fine','set','fit'].indexOf(elem.value.toLowerCase()) !== -1){
+            setTimeout(function (){
+
+                chatBox.innerHTML += "<li class='left-side'> Mei Apki Kia Madad Krskhta hun</li>"
+                typing.style.display = "none";
+            }, 1500)
+        }
+
+
+        else if (['thek','thk','thik','theek', 'alhamdulillah','mast','fine','set','fit'].indexOf(elem.value.toLowerCase()) !== -1){
+            setTimeout(function (){
+
+                chatBox.innerHTML += "<li class='left-side'> Mei Apki Kia Madad Krskhta hun</li>"
+                typing.style.display = "none";
+            }, 1500)
+        }
+
+
+
+
+
+
+
+
+
         else{
             setTimeout(function (){
 
-                list.innerHTML += "<li class='left-side'> Mei samjh nahi sakha wapsi, durust likhye.</li>"
+                chatBox.innerHTML += "<li class='left-side'> Mei samjh nahi sakha wapsi, durust likhye.</li>"
             }, 1500)
         }
 
